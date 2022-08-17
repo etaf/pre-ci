3c3f7d36805c149212bbbc12b325a292317a8ce0
xinan.lin <xinan.lin@intel.com>
2022-08-17 08:35:00 +0800

    test format

---
+++ t.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<map>
+#include<set>
+#include<stack>
+#include<queue>
+#include<limits.h>
+#include<unordered_map>
+#include<unordered_set>
+#include<cmath>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+int x;
+  scanf("%d", &x);
+if (x == 1) {
+  printf("x");
+}
+      else{
+        printf("y");
+      }
+    return 0;
+}
+
+
